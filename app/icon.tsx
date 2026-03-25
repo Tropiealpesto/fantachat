e0208246f1c02a802ed7c559781758313cd70047
Pietro Parodi <pietrparod@gmail.com>
2026-03-25 22:30:50 +0100

    sistemare logo app

---
+++ app/icon.tsx
@@ -0,0 +1,52 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 512,
+  height: 512,
+};
+
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          background: "#ffffff",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 100,
+        }}
+      >
+        <div
+          style={{
+            fontSize: 180,
+            fontWeight: 900,
+            color: "#1a7a3c",
+            lineHeight: 1,
+          }}
+        >
+          Fanta
+        </div>
+
+        <div
+          style={{
+            fontSize: 180,
+            fontWeight: 900,
+            color: "#ff7a20",
+            lineHeight: 1,
+          }}
+        >
+          Chat
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
