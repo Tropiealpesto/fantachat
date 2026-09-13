7fba3436d69f828bbb4239cb6072d3d3115ad831
Pietro Parodi <pietrparod@gmail.com>
2026-09-13 21:24:35 +0200

    sistemazione logo

--- app/components/LogoMark.tsx
+++ app/components/LogoMark.tsx
@@ -3,12 +3,12 @@
 export default function LogoMark({ size = 34 }: { size?: number }) {
   return (
     <img
-      src="/icons/icon-192.png"
+      src="/icons/fantachat-mark.png"
       width={size}
       height={size}
       alt=""
       aria-hidden="true"
-      style={{ display: "block", flexShrink: 0, objectFit: "cover" }}
+      style={{ display: "block", flexShrink: 0, objectFit: "contain" }}
     />
   );
 }
